b97ae0735cf5d4180e591adf806075fe9af5e719
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2018-09-05 19:57:35 -0300

    undo

--- cli/newassignnotification.php
+++ cli/newassignnotification.php
@@ -125,6 +125,7 @@ $querysubmissions = "SELECT CONCAT(us.id,'.',a.id) AS userassign,
 		INNER JOIN {facebook_user} AS fb ON (fb.moodleid = us.id AND fb.status = ?)
 		WHERE a.duedate > ?
 		AND fb.facebookid IS NOT NULL
+		AND asub.status = ?
 		
 		GROUP BY us.id, a.id, c.id
 		ORDER BY us.id";
@@ -140,7 +141,8 @@ $paramsassignment = array(
 $paramsubmission = array(
 		MODULE_ASSIGN,
 		FACEBOOK_COURSE_MODULE_VISIBLE,
-		$initialtime
+		$initialtime,
+		"submitted"
 );
 
 $arraynewassignments = array();
